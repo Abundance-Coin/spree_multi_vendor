2257c23fc4cfde92a57688dad843481eecc82a6a
Artem Kozaev <artemkozaev@gmail.com>
2018-02-08 16:23:05 +0300

    Show users assigned to vendors in admin pane [fixes #153208499]

--- app/helpers/spree/vendor_helper.rb
+++ app/helpers/spree/vendor_helper.rb
@@ -3,6 +3,7 @@ module Spree
     private
 
     def current_spree_vendor
+      return unless current_spree_user
       # disallow current_spree_vendor to include admins
       current_spree_user.vendors.first if !current_spree_user.respond_to?(:has_spree_role?) || !current_spree_user.has_spree_role?(:admin)
     end
