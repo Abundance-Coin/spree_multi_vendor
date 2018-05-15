67453d860b8442f0cc9833222223f61e8e6c1267
Artem Kozaev <artemkozaev@gmail.com>
2018-05-15 19:17:59 +0300

    Changes markup amount field precision and scale

--- db/migrate/20180514123018_create_spree_price_markups.rb
+++ db/migrate/20180514123018_create_spree_price_markups.rb
@@ -1,7 +1,7 @@
 class CreateSpreePriceMarkups < ActiveRecord::Migration[5.1]
   def change
     create_table :spree_price_markups do |t|
-      t.decimal :amount, precision: 8, scale: 5
+      t.decimal :amount, precision: 10, scale: 2
       t.string :name
       t.references :vendor
 
