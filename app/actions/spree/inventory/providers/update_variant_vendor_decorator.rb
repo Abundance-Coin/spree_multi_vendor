c0bdb2dfc82ef01df4f19f5d3ccdeee7a655dd84
Artem Kozaev <artemkozaev@gmail.com>
2018-07-12 17:46:51 +0300

    Update spree_batch_api dependency

--- app/actions/spree/inventory/providers/update_variant_vendor_decorator.rb
+++ app/actions/spree/inventory/providers/update_variant_vendor_decorator.rb
@@ -11,8 +11,8 @@ module Spree
           Variant.unscoped.where(sku: item[:sku], product: product, vendor_id: options[:vendor_id]).first_or_initialize
         end
       end
+
+      DefaultVariantProvider.prepend(UpdateVariantVendorDecorator)
     end
   end
 end
-
-Spree::Inventory::Providers::DefaultVariantProvider.prepend(Spree::Inventory::Providers::UpdateVariantVendorDecorator)
--- spec/support/spree_batch_api.rb
+++ spec/support/spree_batch_api.rb
@@ -1,5 +1,5 @@
 RSpec.configure do |config|
   config.before do
-    allow_any_instance_of(Spree::Inventory::Providers::Books::MetadataProvider).to receive(:images).and_return nil
+    allow_any_instance_of(Spree::Inventory::Providers::Fake::MetadataProvider).to receive(:images).and_return nil
   end
 end
