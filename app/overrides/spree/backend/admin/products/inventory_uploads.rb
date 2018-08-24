ccb36e768d849f0d38c1a0e8a6a6ebd0c3ed43fc
Artem Kozaev <artemkozaev@gmail.com>
2018-08-24 17:34:06 +0300

    Add vendor to product uploads table

---
+++ app/overrides/spree/backend/admin/products/inventory_uploads.rb
@@ -0,0 +1,19 @@
+Deface::Override.new(
+  virtual_path: 'spree/admin/products/inventory_uploads',
+  name: 'Add vendor to product uploads table head',
+  insert_bottom: 'tr[data-hook="admin_product_uploads_headers"]',
+  text:       <<-HTML
+                <th>Vendor</th>
+              HTML
+)
+
+Deface::Override.new(
+  virtual_path: 'spree/admin/products/inventory_uploads',
+  name: 'Add vendor to product uploads table body',
+  insert_bottom: 'tr[data-hook="admin_product_uploads_rows"]',
+  text:       <<-HTML
+                <td>
+                  <%= upload.vendor.presentation_or_name %>
+                </td>
+              HTML
+)
