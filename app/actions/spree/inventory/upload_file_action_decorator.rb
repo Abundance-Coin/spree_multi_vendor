57caf937b389e715af3d0a5dbd63382e5103087a
Nick Zhebrun <nikolay.zhebrun@gmail.com>
2018-09-05 22:01:03 +0300

    Change queue_name getter

--- app/actions/spree/inventory/upload_file_action_decorator.rb
+++ app/actions/spree/inventory/upload_file_action_decorator.rb
@@ -2,7 +2,11 @@ module Spree
   module Inventory
     module UploadFileActionDecorator
       def upload_options
-        { vendor_id: upload_meta.delete(:vendor_id) }
+        @upload_options ||= { vendor_id: upload_meta.delete(:vendor_id) }
+      end
+
+      def queue_name
+        Spree::Vendor.find(upload_options[:vendor_id]).slug
       end
     end
 
